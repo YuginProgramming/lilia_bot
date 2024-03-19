0485c243e632ab313737cbbf1a0a9824e796706b
YuginProgramming <dudaryev@gmail.com>
2024-03-19 17:31:06 +0200

    ready to use

--- sendMessage.js
+++ sendMessage.js
@@ -8,12 +8,16 @@ export const sendInfo = () => {
         if (msg.text == 'send') {
             try {
                 const info = await getData(dataBot.googleSheetId, 'lilia');
-                await admin.sendMessage(dataBot.channelId, info);
-
+                
+                // Start the iteration from the second element of the info array
+                for (let i = 1; i < info.length; i++) {
+                    const row = info[i];
+                    const message = row.join('\n');
+                    await admin.sendMessage(dataBot.channelId, message);
+                }
             } catch (error) {
                 console.error("Error fetching data from Google Sheet:", error);
             }
         }
     });
 };
-
