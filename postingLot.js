1559c01994772383b573b13ca88de370c77f89c5
YuginProgramming <dudaryev@gmail.com>
2024-03-29 16:17:28 +0200

    update

--- postingLot.js
+++ postingLot.js
@@ -1,16 +1,18 @@
-import { readGoogle } from './crud';
-import { dataBot, ranges } from './values';
+import { dataBot, ranges } from './values.js';
+import { admin } from './app.js';
+import { writeGoogle, readGoogle } from './crud.js';
 
 const getLotContentByID = async (lotNumber) => {
     const content = await readGoogle(ranges.postContentLine(lotNumber));
     const message = `\u{1F4CA} ${content[0]} \n ${content[1]} \n ${content[2]} \n ${content[3]} \n \u{1F69C} ${content[4]}`;
     return message;
 };
-
-const autoPosting = async () => {
+// автоматично через 5с шле повідомлення в групу
+export const autoPosting = async () => {
+// вичитує статус неопублікованих повідомлень
     const statusValues = await readGoogle(ranges.statusColumn);
     const pendingLots = statusValues
-      .map((value, index) => value === "FALSE" ? index + 1 : null)
+      .map((value, index) => value === "pending" ? index + 1 : null)
       .filter(value => value !== null);
     //сюди дійшов
     const contentPromises = pendingLots.map(el => getLotContentByID(el));
@@ -19,21 +21,22 @@ const autoPosting = async () => {
       const element = lotsContent[index];
       const lotNumber = pendingLots[index];
       try {
-        const postedLot = await bot.sendMessage(dataBot.channelId, element, { reply_markup: keyboards.channelKeyboard });
-        await sendLotToRegistredCustomers(element, lotNumber);
+        const postedLot = await admin.sendMessage(dataBot.channelId, element);
         if (postedLot) {
           try {
-            const statusChangeResult = await writeGoogle(ranges.statusCell(lotNumber), [['new']]);
+            const statusChangeResult = await writeGoogle(ranges.statusCell(lotNumber), [['posted']]);
+            //console.log('ranges.statusCell(lotNumber):', ranges.statusCell(lotNumber));
+            //запис id message у google
             const postingMessageIDResult = await writeGoogle(ranges.message_idCell(lotNumber), [[postedLot.message_id]]);
             if (statusChangeResult && postingMessageIDResult) {
-              logger.info(`Lot #${lotNumber} successfully posted`);  
+              console.log(`Lot #${lotNumber} successfully posted`);  
             }
           } catch (error) {
-            logger.warn(`Lot #${lotNumber} posted. But issues with updating sheet. !PLEASE CHECK! spreadsheet data. Error ${error}`);
+            console.log(`Lot #${lotNumber} posted. But issues with updating sheet. !PLEASE CHECK! spreadsheet data. Error ${error}`);
           }
         }
       } catch (error) {
-        logger.warn(`Something went wrong on autoposting lot #${lotNumber}. Error ${error}`);
+        console.log(`Something went wrong on autoposting lot #${lotNumber}. Error ${error}`);
       }
     }
   };
--- readMessage.js
+++ readMessage.js
@@ -1,14 +1,24 @@
 import { admin } from './app.js';
-//import { dataBot } from './values.js';
+import { dataBot, ranges } from './values.js';
+import { writeGoogle, readGoogle } from './crud.js';
 
-export const readAndLogMessages = () => {
-    admin.on('message', (msg) => {
+export const readAndLogMessages =  () => {
+    admin.on('message', async (msg) => {
         const chatId = msg.chat.id;
         const messageText = msg.text;
 
         if (msg.reply_to_message) {
             const replyMessageId = msg.reply_to_message.message_id;
             const replyMessageText = msg.reply_to_message.text;
+            //await writeGoogle(ranges.replyStatusCell(lotNumber), [['TRUE']]);
+
+            const statusValues = await readGoogle(ranges.contentColumn);
+            const pendingLots = statusValues
+                .map((value, index) => value === replyMessageText ? index + 1 : null)
+                .filter(value => value !== null);
+
+            console.log('pendingLots:', pendingLots);
+
             console.log(`Received a reply to message ID ${replyMessageId}: "${replyMessageText}"`);
         } else {
             console.log('Received an independent message');
