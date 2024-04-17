c915a77c40d89e741bdc3edd0a4aa5bef118f109
YuginProgramming <dudaryev@gmail.com>
2024-04-17 07:40:52 +0300

    checkboxstop

--- readMessage.js
+++ readMessage.js
@@ -16,7 +16,7 @@ export const readMessages = () => {
                 const lotNumber = messageIdValues.findIndex(value => parseInt(value) === replyMessageId);
 
                 if (lotNumber !== -1) {
-                    await writeGoogle(ranges.replyStatusCell(lotNumber + 1), [['TRUE']]);
+                    //await writeGoogle(ranges.replyStatusCell(lotNumber + 1), [['TRUE']]);
                     await writeGoogle(ranges.replyAnswerColumn(lotNumber + 1), [[messageText]]);
                 } else {
                     console.log(`Message ID ${replyMessageId} not found in the database.`);
